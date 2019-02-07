7e3fa45fafc2b187e6090d05b767ff353f2ac83b
Ilya Kopyl <ilya.kopyl@gmail.com>
2019-02-06 20:27:58 -0800

    Some function craziness

--- node/functions.js
+++ node/functions.js
@@ -2,6 +2,7 @@
 function hi() {
   console.log('Say Hello');
 }
+// try to avoid using function, use arrow functions instead
 
 hi();
 
@@ -15,5 +16,31 @@ arrow('hi')
 // for practice
 
 // make an arrow function with no block body
+const myArrow = (y) => y + 1;
+const myArrow2 = (y) => {};
+
+console.log(myArrow(252));
+console.log(myArrow2(252));
 
 // make a function that returns a function
+const functionFactory = () => () => console.log('Hello, world!');
+
+const a = functionFactory();
+a();
+
+// or so, albeit not recommended
+const b = functionFactory()();
+
+
+const anotherFunction = functionFactory;
+anotherFunction()();
+
+// good practice – declare your functions with const
+
+let d = 7;
+let c = d;
+c++;
+console.log(d);
+
+
+
