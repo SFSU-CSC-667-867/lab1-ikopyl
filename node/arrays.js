2b6bfb6c99619a583106e56605ca1377cc48ec39
Ilya Kopyl <ilya.kopyl@gmail.com>
2019-02-06 20:37:50 -0800

    working with arrays

--- node/arrays.js
+++ node/arrays.js
@@ -3,6 +3,11 @@
 // arrays can be delared explicitly
 const array = [9, 3, 4];
 
+
+const arrayTest = [9, 3, 4];
+arrayTest[5] = 9;
+console.log(arrayTest);
+
 // or with new keyword
 const anotherArray = new Array(5);
 
@@ -15,14 +20,36 @@ array[1] = 'hello';
 // for practice
 
 // use forEach
+array.forEach((n) => {
+    console.log(n);
+});
 
+console.log('------------');
 // use map
+const newArray = arrayTest.map((i) => i + 1);
+
+newArray.forEach((n) => {
+    console.log(n);
+});
 
 // use pop
+newArray.pop();
+console.log(newArray);
+
+newArray.pop();
+newArray.pop();
+console.log(newArray);
+
+console.log('---- push: --------');
 
 // use push
+newArray.push(6);
+console.log(newArray);
 
+console.log('---- shift: --------');
 // use shift
+newArray.shift();
+console.log(newArray);
 
 // use unshift
 
